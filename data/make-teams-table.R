d9b0c2b4a130f8657732629f376b7326fd9933d6
Yin Chen Wan <yinchen202009@gmail.com>
2018-09-27 18:14:42 -0700

    deleted this extra file

--- data/make-teams-table.R
+++
@@ -1,9 +0,0 @@
-#title: Data preparation 
-
-#description: Create a csv data file that will contain 
-#the required variables to be used in the ranking analysis.
-
-#input: nba2018.csv
-#output: nba2018-teams.csv
-
-#A bit of preprocessing
